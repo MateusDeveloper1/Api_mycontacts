0a6a383cbfae18780f8b5ca2a234ce551293e476
Mateus Silva <mateusilva767@gmail.com>
2023-11-10 18:10:00 -0300

    project created and database integration created

---
+++ src/app/repositories/ContactRepository.js
@@ -0,0 +1,95 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Mateus',
+    email: 'mateus@gmail.com',
+    phone: '112154853',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'José',
+    email: 'jose@gmail.com',
+    phone: '13565554853',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Rose',
+    email: 'rose@gmail.com',
+    phone: '112155464653',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.id === id),
+      );
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.email === email),
+      );
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updatedContact : contact
+      ));
+
+      resolve(updatedContact);
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
